6fd4f49fa37dd97a12db375cbb2e9d92570f0c0b
zeng her <zcher82@Zengs-MacBook-Pro.local>
2016-06-07 17:14:24 -0500

    updated a couple more views and controllers before branching to work on navigation/menu hamburger

--- server/public/scripts/clientapp.js
+++ server/public/scripts/clientapp.js
@@ -1,1 +1,33 @@
 var app = angular.module('myApp', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/home', {
+      templateUrl: '/views/home.html',
+      controller: 'HomeController'
+    })
+    .when('/accounts', {
+      templateUrl: '/views/accounts.html',
+      controller: 'AccountsController'
+    })
+    .when('/color', {
+      templateUrl: '/views/color.html',
+      controller: 'ColorController'
+    })
+    .when('/floralaccents', {
+      templateUrl: '/views/floralaccents.html',
+      controller: 'FloralAccentsController'
+    })
+    .when('/flowers', {
+      templateUrl: '/views/flowers.html',
+      controller: 'FlowersController'
+    })
+    .when('/gallery', {
+      templateUrl: '/views/gallery.html',
+      controller: 'GalleryController'
+    })
+    .otherwise({
+      redirectTo: '/home'
+    })
+
+}]);
---
+++ server/public/scripts/controllers/flowerscontroller.js
@@ -0,0 +1,14 @@
+app.controller('FlowersController', ['$scope', '$http', function($scope, $http) {
+  console.log('FlowersController running');
+
+  $scope.flowers = [];
+
+  getFlowers();
+
+  function getFlowers() {
+    $http.get('/accents')
+      .then(function(response){
+        console.log(response);
+        scope.floralAccents = response.data;
+      });
+  }

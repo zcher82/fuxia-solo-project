7b8eb1aff044ff82f5a3576b8ef56a7b22db22b1
zeng her <zcher82@gmail.com>
2016-06-12 10:41:31 -0500

    got flowers page display working. will work on function for displaying info and showing all images when sample photo is clicked.

--- server/public/scripts/controllers/flowerscontroller.js
+++ server/public/scripts/controllers/flowerscontroller.js
@@ -2,14 +2,31 @@ app.controller('FlowersController', ['$scope', '$http', function($scope, $http) 
   console.log('FlowersController running');
 
   $scope.flowers = [];
+  $scope.samples = [];
+  $scope.photos = [];
 
   getFlowers();
 
   function getFlowers() {
-    $http.get('/accents')
-      .then(function(response){
-        console.log(response);
-        scope.floralAccents = response.data;
+    $http.get('/flowerdb')
+      .then(function (response) {
+        $scope.flowers = response.data;
+
+        /*console.log('GET /flowers ', response.data);*/
+
+        angular.forEach($scope.flowers, function (value) { //for each object(flower) in the returned array, do this function
+          console.log(value.images[0]);
+          $scope.samples.push(value.images[0]); //pushing ONLY the FIRST image of each object to $scope.samples array
+
+          //now we go through each original flower object to push ALL the photos
+          //into $scope.photos array to use with preview functiion
+          angular.forEach(value.images, function (value) {
+            $scope.photos.push(value);
+            console.log($scope.photos);
+          });
+        });
+
+
       });
   }
 

95a4aef38f0da321d70d3306e831b35ce8fb8234
zeng her <zcher82@gmail.com>
2016-06-17 17:05:10 -0500

    trying to find why code is broken in gallery before pushing to heroku

--- server/public/scripts/controllers/gallerycontroller.js
+++ server/public/scripts/controllers/gallerycontroller.js
@@ -5,17 +5,6 @@ app.controller('GalleryController', ['$scope', '$http', function($scope, $http) 
   $scope.arrowNext = "arrowNext";
   $scope.arrangements = [];
   getArrangements();
-//$scope.photos = $scope.arrangements
-
-/*  // Set of Photos
-  $scope.photos = [
-      {src: 'http://farm9.staticflickr.com/8042/7918423710_e6dd168d7c_b.jpg', desc: 'Image 01'},
-      {src: 'http://farm9.staticflickr.com/8449/7918424278_4835c85e7a_b.jpg', desc: 'Image 02'},
-      {src: 'http://farm9.staticflickr.com/8457/7918424412_bb641455c7_b.jpg', desc: 'Image 03'},
-      {src: 'http://farm9.staticflickr.com/8179/7918424842_c79f7e345c_b.jpg', desc: 'Image 04'},
-      {src: 'http://farm9.staticflickr.com/8315/7918425138_b739f0df53_b.jpg', desc: 'Image 05'},
-      {src: 'http://farm9.staticflickr.com/8461/7918425364_fe6753aa75_b.jpg', desc: 'Image 06'}
-  ];*/
 
 
   function getArrangements() {
